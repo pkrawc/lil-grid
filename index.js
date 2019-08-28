3e7b397245d90e3866cae7b56037b32e347e77ac
Patrick Krawczykowski <patrick@dreadful.design>
2019-08-28 10:48:27 -0500

    fix unexpected token

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const StyledGrid = styled.section`
 const Grid = ({ children, theme, ...props }) => {
   const breakPoints = {
     ...theme.breakPoints
-  },
+  }
   const renderChildren = () =>
     React.Children.map(children, child =>
       React.cloneElement(child, {
@@ -36,24 +36,41 @@ Grid.defaultProps = {
 
 const getIndex = (arr, idx) => arr[idx] || arr[arr.length - 1]
 
-const getOffset = (arr, idx) => (getIndex(arr, idx) === "auto" ? "auto" : getIndex(arr, idx) + 1)
+const getOffset = (arr, idx) =>
+  getIndex(arr, idx) === "auto" ? "auto" : getIndex(arr, idx) + 1
 
 const Col = styled.div`
-  grid-column: ${({ offset }) => getOffset(offset, 0)} / span ${({ span }) => getIndex(span, 0)};
-  @media (min-width: ${({ breakPoints: { xsm } }) => { xsm }}rem) {
-    grid-column: ${({ offset }) => getOffset(offset, 1)} / span ${({ span }) => getIndex(span, 1)};
+  grid-column: ${({ offset }) => getOffset(offset, 0)} / span
+    ${({ span }) => getIndex(span, 0)};
+  @media (min-width: ${({ breakPoints: { xsm } }) => {
+      xsm
+    }}rem) {
+    grid-column: ${({ offset }) => getOffset(offset, 1)} / span
+      ${({ span }) => getIndex(span, 1)};
   }
-  @media (min-width: ${({ breakPoints: { sm } }) => { sm }}rem) {
-    grid-column: ${({ offset }) => getOffset(offset, 2)} / span ${({ span }) => getIndex(span, 2)};
+  @media (min-width: ${({ breakPoints: { sm } }) => {
+      sm
+    }}rem) {
+    grid-column: ${({ offset }) => getOffset(offset, 2)} / span
+      ${({ span }) => getIndex(span, 2)};
   }
-  @media (min-width: ${({ breakPoints: { md } }) => { md }}rem) {
-    grid-column: ${({ offset }) => getOffset(offset, 3)} / span ${({ span }) => getIndex(span, 3)};
+  @media (min-width: ${({ breakPoints: { md } }) => {
+      md
+    }}rem) {
+    grid-column: ${({ offset }) => getOffset(offset, 3)} / span
+      ${({ span }) => getIndex(span, 3)};
   }
-  @media (min-width: ${({ breakPoints: { lg } }) => { lg }}rem) {
-    grid-column: ${({ offset }) => getOffset(offset, 4)} / span ${({ span }) => getIndex(span, 4)};
+  @media (min-width: ${({ breakPoints: { lg } }) => {
+      lg
+    }}rem) {
+    grid-column: ${({ offset }) => getOffset(offset, 4)} / span
+      ${({ span }) => getIndex(span, 4)};
   }
-  @media (min-width: ${({ breakPoints: { xlg } }) => { xlg }}rem) {
-    grid-column: ${({ offset }) => getOffset(offset, 5)} / span ${({ span }) => getIndex(span, 5)};
+  @media (min-width: ${({ breakPoints: { xlg } }) => {
+      xlg
+    }}rem) {
+    grid-column: ${({ offset }) => getOffset(offset, 5)} / span
+      ${({ span }) => getIndex(span, 5)};
   }
 `
 
