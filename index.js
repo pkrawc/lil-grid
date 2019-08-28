9397d0e0b8d4aa2854ad1c49dd15ebfb88ae3dea
Patrick Krawczykowski <patrick@dreadful.design>
2019-08-28 08:33:22 -0500

    fix exports

--- index.js
+++ index.js
@@ -7,22 +7,17 @@ const StyledGrid = styled.section`
   grid-gap: ${({ gap }) => gap};
 `
 
-export const Grid = ({
-  children,
-  xsmBreak,
-  smBreak,
-  mdBreak,
-  lgBreak,
-  xlgBreak,
-  ...props
-}) => {
-  const renderChildren = () => React.Children.map(children, child => React.cloneElement(child, {
-    xsmBreak,
-    smBreak,
-    mdBreak,
-    lgBreak,
-    xlgBreak
-  }))
+const Grid = ({ children, xsmBreak, smBreak, mdBreak, lgBreak, xlgBreak, ...props }) => {
+  const renderChildren = () =>
+    React.Children.map(children, child =>
+      React.cloneElement(child, {
+        xsmBreak,
+        smBreak,
+        mdBreak,
+        lgBreak,
+        xlgBreak
+      })
+    )
   return <StyledGrid {...props}>{renderChildren()}</StyledGrid>
 }
 
@@ -42,19 +37,19 @@ const getOffset = (arr, idx) => (getIndex(arr, idx) === "auto" ? "auto" : getInd
 
 const Col = styled.div`
   grid-column: ${({ offset }) => getOffset(offset, 0)} / span ${({ span }) => getIndex(span, 0)};
-  @media (min-width: ${({xsmBreak}) => xsmBreak}rem) {
+  @media (min-width: ${({ xsmBreak }) => xsmBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 1)} / span ${({ span }) => getIndex(span, 1)};
   }
-  @media (min-width: ${({smBreak}) => smBreak}rem) {
+  @media (min-width: ${({ smBreak }) => smBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 2)} / span ${({ span }) => getIndex(span, 2)};
   }
-  @media (min-width: ${({mdBreak}) => mdBreak}rem) {
+  @media (min-width: ${({ mdBreak }) => mdBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 3)} / span ${({ span }) => getIndex(span, 3)};
   }
-  @media (min-width: ${({lgBreak}) => lgBreak}rem) {
+  @media (min-width: ${({ lgBreak }) => lgBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 4)} / span ${({ span }) => getIndex(span, 4)};
   }
-  @media (min-width: ${({xlgBreak}) => xlgBreak}rem) {
+  @media (min-width: ${({ xlgBreak }) => xlgBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 5)} / span ${({ span }) => getIndex(span, 5)};
   }
 `
