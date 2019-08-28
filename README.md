a24c8b52cd2922eeffdb4ea7eaa38369d85c34f7
Patrick Krawczykowski <patrick@dreadful.design>
2019-08-28 08:27:07 -0500

    add grid breakpoint props

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Latticework
+# Lil Grid
 
 A _tiny_ grid for `styled-components` systems.
+
+## Anatomy
+
+Tiny grid uses `rem` units to give a consistency within your design. You can should be setting the `rem` in your project to a spacing that makes sense for you.
 
 ## Usage
 
@@ -37,3 +41,22 @@ export default () => {
   </Grid>
 }
 
+
+## Props
+
+### Grid
+| Name     | Required | Default | Description                        |
+| -------- | -------- | ------- | ---------------------------------- |
+| gap      | false    | 2       | Grid gap between columns and rows. |
+| columns  | false    | 12      | Number of columns in the grid.     |
+| xsmBreak | false    | 40      | Extra small breakpoint             |
+| smBreak  | false    | 60      | Small breakpoint                   |
+| mdBreak  | false    | 80      | Medium breakpoint                  |
+| lgBreak  | false    | 100     | Large breakpoint                   |
+| xlgBreak | false    | 120     | Extra large breakpoint             |
+
+### Col
+| Name   | Required | Default  | Description                                 |
+| ------ | -------- | -------- | ------------------------------------------- |
+| span   | true     | [12]     | An array of lenghts for the column to span. |
+| offset | false    | ["auto"] | An array of offsets for the column.         |
--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+import React from "react"
 import styled from "styled-components"
 
-const Grid = styled.section`
+const StyledGrid = styled.section`
   display: grid;
   grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
-  grid-gap: 2rem;
+  grid-gap: ${({ gap }) => gap};
 `
 
+export const Grid = ({
+  children,
+  xsmBreak,
+  smBreak,
+  mdBreak,
+  lgBreak,
+  xlgBreak,
+  ...props
+}) => {
+  const renderChildren = () => React.Children.map(children, child => React.cloneElement(child, {
+    xsmBreak,
+    smBreak,
+    mdBreak,
+    lgBreak,
+    xlgBreak
+  }))
+  return <StyledGrid {...props}>{renderChildren()}</StyledGrid>
+}
+
 Grid.defaultProps = {
-  columns: 12
+  columns: 12,
+  gap: 2,
+  xsmBreak: 40,
+  smBreak: 60,
+  mdBreak: 80,
+  lgBreak: 100,
+  xlgBreak: 120
 }
 
 const getIndex = (arr, idx) => arr[idx] || arr[arr.length - 1]
@@ -16,17 +42,20 @@ const getOffset = (arr, idx) => (getIndex(arr, idx) === "auto" ? "auto" : getInd
 
 const Col = styled.div`
   grid-column: ${({ offset }) => getOffset(offset, 0)} / span ${({ span }) => getIndex(span, 0)};
-  @media (min-width: 40rem) {
+  @media (min-width: ${({xsmBreak}) => xsmBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 1)} / span ${({ span }) => getIndex(span, 1)};
   }
-  @media (min-width: 60rem) {
+  @media (min-width: ${({smBreak}) => smBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 2)} / span ${({ span }) => getIndex(span, 2)};
   }
-  @media (min-width: 80rem) {
+  @media (min-width: ${({mdBreak}) => mdBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 3)} / span ${({ span }) => getIndex(span, 3)};
   }
-  @media (min-width: 100rem) {
+  @media (min-width: ${({lgBreak}) => lgBreak}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 4)} / span ${({ span }) => getIndex(span, 4)};
+  }
+  @media (min-width: ${({xlgBreak}) => xlgBreak}rem) {
+    grid-column: ${({ offset }) => getOffset(offset, 5)} / span ${({ span }) => getIndex(span, 5)};
   }
 `
 
