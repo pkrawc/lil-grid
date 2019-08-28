59e9339f62c83aaa0a77f118fbc3b9f2ee2ddd54
Patrick Krawczykowski <patrick@dreadful.design>
2019-08-28 10:43:47 -0500

    add support for theming

--- index.js
+++ index.js
@@ -3,32 +3,35 @@ import styled from "styled-components"
 
 const StyledGrid = styled.section`
   display: grid;
-  grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
-  grid-gap: ${({ gap }) => gap};
+  grid-template-columns: repeat(${({ theme }) => theme.columns}, 1fr);
+  grid-gap: ${({ theme }) => theme.gap};
 `
 
-const Grid = ({ children, xsmBreak, smBreak, mdBreak, lgBreak, xlgBreak, ...props }) => {
+const Grid = ({ children, theme, ...props }) => {
+  const breakPoints = {
+    ...theme.breakPoints
+  },
   const renderChildren = () =>
     React.Children.map(children, child =>
       React.cloneElement(child, {
-        xsmBreak,
-        smBreak,
-        mdBreak,
-        lgBreak,
-        xlgBreak
+        breakPoints
       })
     )
   return <StyledGrid {...props}>{renderChildren()}</StyledGrid>
 }
 
 Grid.defaultProps = {
-  columns: 12,
-  gap: 2,
-  xsmBreak: 40,
-  smBreak: 60,
-  mdBreak: 80,
-  lgBreak: 100,
-  xlgBreak: 120
+  theme: {
+    columns: 12,
+    gap: 2,
+    breakPoints: {
+      xsm: 40,
+      sm: 60,
+      md: 80,
+      lg: 100,
+      xlg: 120
+    }
+  }
 }
 
 const getIndex = (arr, idx) => arr[idx] || arr[arr.length - 1]
@@ -37,19 +40,19 @@ const getOffset = (arr, idx) => (getIndex(arr, idx) === "auto" ? "auto" : getInd
 
 const Col = styled.div`
   grid-column: ${({ offset }) => getOffset(offset, 0)} / span ${({ span }) => getIndex(span, 0)};
-  @media (min-width: ${({ xsmBreak }) => xsmBreak}rem) {
+  @media (min-width: ${({ breakPoints: { xsm } }) => { xsm }}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 1)} / span ${({ span }) => getIndex(span, 1)};
   }
-  @media (min-width: ${({ smBreak }) => smBreak}rem) {
+  @media (min-width: ${({ breakPoints: { sm } }) => { sm }}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 2)} / span ${({ span }) => getIndex(span, 2)};
   }
-  @media (min-width: ${({ mdBreak }) => mdBreak}rem) {
+  @media (min-width: ${({ breakPoints: { md } }) => { md }}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 3)} / span ${({ span }) => getIndex(span, 3)};
   }
-  @media (min-width: ${({ lgBreak }) => lgBreak}rem) {
+  @media (min-width: ${({ breakPoints: { lg } }) => { lg }}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 4)} / span ${({ span }) => getIndex(span, 4)};
   }
-  @media (min-width: ${({ xlgBreak }) => xlgBreak}rem) {
+  @media (min-width: ${({ breakPoints: { xlg } }) => { xlg }}rem) {
     grid-column: ${({ offset }) => getOffset(offset, 5)} / span ${({ span }) => getIndex(span, 5)};
   }
 `
